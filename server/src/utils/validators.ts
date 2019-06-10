f4cf27c2af5c8306cb78943cba3dfd77c3ae6553
Muhamad Irham Prasetyo <muhamad.prasetyo@enerxess.com>
2019-06-10 11:20:41 +0200

    validate any

--- server/src/utils/validators.ts
+++ server/src/utils/validators.ts
@@ -4,29 +4,24 @@ export const ValidatorRegisterInput = (
   password: string,
   confirmPassword: string
 ): any => {
-  const errors = {};
+  const errors: any = {};
 
   if (username.trim() === "") {
-    // @ts-ignore
     errors.username = "Username must not be empty";
   }
 
   if (email.trim() === "") {
-    // @ts-ignore
     errors.email = "email must not be empty";
   } else {
     const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
     if (!email.match(regEx)) {
-      // @ts-ignore
       errors.email = "email must be a valid address";
     }
   }
 
   if (password === "") {
-    // @ts-ignore
     errors.password = "Password must not be empty";
   } else if (password !== confirmPassword) {
-    // @ts-ignore
     errors.confirmPassword = "Password must match";
   }
 
@@ -35,15 +30,13 @@ export const ValidatorRegisterInput = (
 };
 
 export const ValidatorLoginInput = (username: string, password: string) => {
-  const errors = {};
+  const errors: any = {};
 
   if (username.trim() === "") {
-    // @ts-ignore
     errors.username = "Username must not be empty";
   }
 
   if (password.trim() === "") {
-    // @ts-ignore
     errors.password = "Password must not be empty";
   }
 
